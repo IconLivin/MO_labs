c32e0bc96c2ae344fb3abb14b5690c0f87bd7192
Kuzhelev, Anton <anton.kuzhelev@intel.com>
2020-11-17 12:12:56 +0300

    Added comments for fools

--- gradient.py
+++ gradient.py
@@ -1,176 +1,125 @@
-from mpl_toolkits.mplot3d import Axes3D
-import scipy as sc
-import sympy
 import numpy
-import pylab
+from sympy import cos, symbols, diff, sin
+from sympy.plotting import plot3d
+import numpy as np
+import matplotlib.pylab as plt
+import scipy.optimize as opt
 import matplotlib.pyplot as plt
-from scipy import optimize
-from sympy.vector import CoordSys3D, gradient
 
-sympy.init_printing()
+"""Если ты читаешь это, то ты ВОР! Не стыдно? Зачем воруешь? Лаааадно. на первый раз прощаю"""
 
 
-def makeData():
-    x, y = numpy.mgrid[-1.5:2:25j, -2:2:25j]
+x1, x2 = symbols("x1 x2")
 
-    z = func(x, y)
-    return x, y, z
+"""Уравнение с листочка"""
+f = 20 * (cos(3 * x1) - x2) ** 2 + (x2 - 4 * x1) ** 2
 
-
-def makeLims():
-    x, y = numpy.mgrid[-1.5:2:25j, -2:2:25j]
-
-    z = [lim1(x, y), lim2(x, y), lim3(x, y)]
-    return z
+"""Ограничения g1,g2,g3"""
+g1 = x2 + x1 - 0.1
+g2 = x2 - x1 - 0.25
+g3 = -1 * (x1 - 1) ** 2 - 2 * (x2 - 1) ** 2 + 5
 
 
-def func(x1, x2):
-    return 20 * (numpy.cos(3 * x1) - x2) ** 2 + (x2 - 4 * x1) ** 2
+"""Ограничения по x,y """
+dx = (-1.5, 2.0)
+dy = (-2.0, 2.0)
 
 
-def grad(x, y):
-    return 32 * x - 8 * y - 120 * (-y + numpy.cos(3 * x)) * numpy.sin(3 * x), (
-        -8 * x + 42 * y - 40 * numpy.cos(3 * x)
+"""Эта часть отделяет допустимую область (та что не в сетке)"""
+x1_, x2_ = np.meshgrid(np.linspace(-1.5, 2.0, 500), np.linspace(-2.0, 2.0, 500))
+
+"""Уравнение с листочка"""
+f_ = 20 * (np.cos(3 * x1_) - x2_) ** 2 + (x2_ - 4 * x1_) ** 2
+
+"""Ограничения g1,g2,g3"""
+g1_ = x2_ + x1_ - 0.1
+g2_ = x2_ - x1_ - 0.25
+g3_ = -1 * (x1_ - 1) ** 2 - 2 * (x2_ - 1) ** 2 + 5
+
+plt.figure()
+plt.pcolormesh(x1_, x2_, (g1_ <= 0) & (g2_ <= 0) & (g3_ <= 0), alpha=0.1)
+contours = plt.contour(x1_, x2_, f_, 25)
+plt.clabel(contours, inline=True, fontsize=8)
+contours = plt.contour(x1_, x2_, g1_, (0,), colors="r")
+contours = plt.contour(x1_, x2_, g2_, (0,), colors="y")
+contours = plt.contour(x1_, x2_, g3_, (0,), colors="g")
+
+
+"""Первое приближение, можно понять по картинке куда сходятся линии"""
+initial_guess = numpy.array([1.1, -1.2])
+
+plt.scatter(initial_guess[0], initial_guess[1], color="r")
+
+"""Уравнения огрниченний где параметр вектор"""
+
+
+def con_g1(x):
+    return x[0] + x[1] - 0.1
+
+
+def con_g2(x):
+    return x[1] - x[0] - 0.25
+
+
+def con_g3(x):
+    return -1 * (x[0] - 1) ** 2 - 2 * (x[1] - 1) ** 2 + 5
+
+
+"""Функция с теми же критериями"""
+
+
+def func(x):
+    return 20 * (cos(3 * x[0]) - x[1]) ** 2 + (x[1] - 4 * x[0]) ** 2
+
+
+"""Активные ограничения(зависит от начального приближения, можно выбрать любой, если поставить два, то он будет стремится найти решение на пересечении)"""
+cons = [{"type": "eq", "fun": con_g3}]
+
+"""Поиск минимума"""
+res = opt.minimize(func, initial_guess, constraints=cons)
+
+print(res.x)
+
+"""Вывод минимума на график линии уровня"""
+plt.scatter(res.x[0], res.x[1])
+
+"""Получение решения по оси z"""
+z = func(res.x)
+
+print("Minimum: ", res.x[0], res.x[1], z)
+
+"""Вычисления градиента функции по x,y"""
+df_dx = [diff(f, x1), diff(f, x2)]
+print(df_dx)
+
+"""Вычисления градиента ограничений по x,y"""
+dg2_dx = [diff(g2, x1), diff(g2, x2)]
+print(dg2_dx)
+
+dg3_dx = [diff(g3, x1), diff(g3, x2)]
+print(dg3_dx)
+
+"""Эта формула напрямую следует из уравнения KKT, -grad F(x) = lambda * grad g3(x); Отсюда выражаем лямбда"""
+
+
+def res1(x1, x2):
+    return (
+        -1 * (32 * x1 - 8 * x2 - 120 * (-x2 + cos(3 * x1)) * sin(3 * x1)) / (2 - 2 * x1)
     )
 
 
-def lim1(x, y):
-    return x + y - 0.1
+"""Тоже что и выше только по y"""
 
 
-def lim2(x, y):
-    return -1 * (x - 1) ** 2 - 2 * (y - 1) ** 2 + 5
+def res2(x1, x2):
+    return -1 * (-8 * x1 + 42 * x2 - 40 * cos(3 * x1)) / (4 - 4 * x2)
 
 
-def lim3(x, y):
-    return y - x - 0.25
+"""Вывод лямбд (не должны быть <0, если меньше - меняйте активные ограничения)"""
+print(res1(*res.x), res2(*res.x))
 
 
-def func1(x):
-    return func(*x)
+"""Вывод графиков"""
+plot3d(f, (x1, dx[0], dx[1]), (x2, dy[0], dy[1]))
 
-
-def cons_f(x):
-    return [-1 * (x[0] - 1) ** 2 - 2 * (x[1] - 1) ** 2]
-
-
-def cons_J(x):
-    return [-2 * (x[0] - 1), -4 * (x[1] - 1)]
-
-
-def make_surface():
-    x = numpy.arange(-1.5, 2, 0.01)
-    y = numpy.arange(-2, 2, 0.01)
-
-    resx = []
-    resy = []
-    for i in x:
-        for j in y:
-            l1 = lim1(i, j)
-            l2 = lim2(i, j)
-            l3 = lim3(i, j)
-            if (
-                l1 <= 0
-                and l2 <= 0
-                and l3 <= 0
-                and abs(max(l1, l2)) - abs(min(l1, l2)) <= numpy.finfo(float).eps
-                and abs(max(l2, l3)) - abs(min(l2, l3)) <= numpy.finfo(float).eps
-                and abs(max(l1, l3)) - abs(min(l1, l3)) <= numpy.finfo(float).eps
-            ):
-                if i in resx:
-                    index = resx.index(i)
-                    if resy[index] < j:
-                        resy[index] = j
-                else:
-                    resx.append(i)
-                    resy.append(j)
-    return resx, resy
-
-
-R = CoordSys3D("")
-
-gradX, gradY = sympy.symbols(("x1 x2"))
-
-s1 = 20 * (sympy.cos(3 * R.x) - R.y) ** 2 + (R.y - 4 * R.x) ** 2
-
-bounds = optimize.Bounds([-1.5, -2], [2, 2])
-
-linear_constraint = optimize.LinearConstraint(
-    [[1, 1], [-1, 1]], [-numpy.inf, -numpy.inf], [0.1, 0.25]
-)
-
-nonlinear_constraint = optimize.NonlinearConstraint(
-    cons_f, -numpy.inf, -5, jac=cons_J, hess="2-point"
-)
-
-_gradient = str(gradient(s1)).replace(".", "")
-
-_gradient = _gradient.replace("**", "^")
-
-_gradient = _gradient.replace("*", " * ")
-
-_gradient = _gradient.replace("/", " / ")
-
-print(_gradient)
-
-x, y, z = makeData()
-
-lims = makeLims()
-
-begin = (0, 1.5)
-
-res = optimize.minimize(
-    func1,
-    x0=begin,
-    method="trust-constr",
-    jac=optimize.rosen_der,
-    hess=optimize.rosen_hess,
-    constraints=[linear_constraint, nonlinear_constraint],
-    options={"verbose": 1},
-    bounds=bounds,
-)
-
-print("Minimum=[", res.x[0], res.x[1], func(res.x[0], res.x[1]), "]")
-f, ax = plt.subplots()
-
-gradx, grady = grad(*begin)
-
-surfx, surfy = make_surface()
-
-lx = numpy.arange(-1.5, 2, 0.1)
-
-ly21 = 1 - numpy.sqrt((5 - (lx - 1) ** 2) / 2)
-ly22 = 1 + numpy.sqrt((5 - (lx - 1) ** 2) / 2)
-ly3 = lx + 0.25
-ly1 = 0.1 - lx
-
-ly4 = numpy.minimum(numpy.abs(ly1), numpy.abs(ly3))
-
-cs = ax.contour(x, y, z, levels=10)
-ax.clabel(cs)
-
-ax.plot(lx, ly1, lx, ly3, linestyle="--")
-
-ax.plot(lx, ly21, lx, ly22, linestyle="--", color="g")
-
-ax.plot(surfx, surfy, color="black")
-
-ax.scatter(res.x[0], res.x[1], func(*res.x))
-# ax.arrow(begin[0], begin[1], -gradx, -grady, head_length = 10)
-
-fig = pylab.figure()
-
-axes = Axes3D(fig)
-
-axes.scatter3D(begin[0], begin[1], func(begin[0], begin[1]), s=50, color="blue")
-axes.scatter3D(res.x[0], res.x[1], func(*res.x), s=100, color="red")
-
-axes.plot_surface(x, y, z, color="green", alpha=0.7)
-
-"""
-for i in range(3):
-    axes.plot_surface(x, y, lims[i], color="yellow", alpha=0.3)
-"""
-
-pylab.show()
 plt.show()
